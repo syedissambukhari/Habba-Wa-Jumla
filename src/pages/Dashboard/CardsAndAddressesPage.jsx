1a24cf6786c369b2801353f5086595621ff333cd
Nabil <cleo.master0000@gmail.com>
2024-10-10 14:40:05 +0500

    Add Modal for Payment Page

--- src/pages/Dashboard/CardsAndAddressesPage.jsx
+++ src/pages/Dashboard/CardsAndAddressesPage.jsx
@@ -2,9 +2,35 @@ import DashboardSider from '../../homecomponents/DashboardSider'
 import PaymentCard from "./component/PaymentCard.jsx";
 import AddressCard from "./component/AddressCard.jsx";
 import {FaArrowRight} from "react-icons/fa6";
+import {useState} from "react";
+import {
+    FloatingFocusManager,
+    FloatingOverlay,
+    useClick,
+    useDismiss,
+    useFloating,
+    useInteractions
+} from "@floating-ui/react";
+import { IoCloseOutline } from "react-icons/io5";
+
 
 
 function CardsAndAddressesPage() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const { refs, context } = useFloating({
+        open: isModalOpen,
+        onOpenChange: setIsModalOpen,
+    })
+
+    const dismiss = useDismiss(context);
+    const click = useClick(context);
+
+    const {getReferenceProps, getFloatingProps} = useInteractions([
+        dismiss,
+        click
+    ]);
+
     return (
         <div className="mt-10 flex gap-8 ">
             <div className="basis w-1/3">
@@ -13,7 +39,10 @@ function CardsAndAddressesPage() {
             <div className="w-full p-6">
                 <div className="mb-6 flex justify-between">
                     <p className="font-medium text-sm leading-[24px]">PAYMENT OPTION</p>
-                    <button className="flex gap-2 items-center">
+                    <button
+                        ref={refs.setReference}
+                        {...getReferenceProps()}
+                        className="flex gap-2 items-center">
                         <span className="text-[#0B5D51] font-[600px] text-sm leading-[24px]">Add Card</span>
                         <FaArrowRight color="#0B5D51"/>
                     </button>
@@ -21,6 +50,7 @@ function CardsAndAddressesPage() {
                 <div className="flex gap-6">
                     <div className="basis-1/3">
                         <PaymentCard
+                            handleEditCard={() => setIsModalOpen(true)}
                             amount="$95, 400.00 USD"
                             cardNumber="**** **** **** 1234"
                             cardHolder="Kevin Gilbert"
@@ -53,6 +83,44 @@ function CardsAndAddressesPage() {
                     />
                 </div>
             </div>
+            {isModalOpen &&
+                <FloatingOverlay
+                lock
+                style={{background: 'rgba(0, 0, 0, 0.8)'}}
+                >
+            <FloatingFocusManager context={context}>
+                <div
+                    ref={refs.setReference}
+                    {...getFloatingProps()}
+                    className="flex w-full h-full items-center justify-center"
+                >
+                    <div className="bg-white p-4 rounded w-1/3">
+                        <div className="mb-8 flex items-center justify-between">
+                            <p className="text-sm font-medium">ADD NEW CARD</p>
+                            <button onClick={() => setIsModalOpen(false)}>
+                                <IoCloseOutline color="gray" size={24} />
+                            </button>
+                        </div>
+                        <p className="font-medium">Name on Card</p>
+                        <input className="p-2 w-full focus:outline-none" type="text"/>
+                        <p className="font-medium">Card Number</p>
+                        <input className="p-2 w-full focus:outline-none" type="text"/>
+                        <div className="flex gap-2 mb-4">
+                            <div>
+                                <p className="font-medium">Expiry Date</p>
+                                <input className="p-2 w-full focus:outline-none" type="text"/>
+                            </div>
+                            <div>
+                                <p className="font-medium">CCV</p>
+                                <input className="p-2 w-full focus:outline-none" type="text"/>
+                            </div>
+                        </div>
+                        <button className="inline-block bg-[#0B5D51] text-white p-4 rounded font-bold">ADD CARD</button>
+                    </div>
+                </div>
+            </FloatingFocusManager>
+                </FloatingOverlay>
+            }
         </div>
     )
 }
--- src/pages/Dashboard/component/PaymentCard.jsx
+++ src/pages/Dashboard/component/PaymentCard.jsx
@@ -2,40 +2,20 @@ import {BsThreeDots} from "react-icons/bs";
 import {PiCopyDuotone} from "react-icons/pi";
 import {RiVisaLine} from "react-icons/ri";
 import { FaCcMastercard } from "react-icons/fa6";
-import { useFloating, useDismiss, useInteractions } from "@floating-ui/react";
+import {useFloating, useDismiss, useInteractions, useClick} from "@floating-ui/react";
 import {useState} from "react";
 
 // eslint-disable-next-line react/prop-types
-function PaymentCard({ amount, cardNumber, cardHolder, cardType,  backgroundColorClassName }) {
-    const [isCardMenuOpen, setIsCardMenuOpen] = useState(false);
-
-    const { refs, floatingStyles, context } = useFloating({
-        open: isCardMenuOpen,
-        onOpenChange: setIsCardMenuOpen,
-        placement: "bottom-start",
-    })
-
-    const dismiss = useDismiss(context);
-
-    const {getReferenceProps, getFloatingProps} = useInteractions([
-        dismiss
-    ])
-
+function PaymentCard({ handleEditCard, amount, cardNumber, cardHolder, cardType,  backgroundColorClassName }) {
 
     return (
         <div className={`p-6 border rounded ${backgroundColorClassName}`}>
             <div className="flex items-center justify-between">
                 <p className="text-white text-[16px] leading-6">{amount}</p>
-                <div
-                    className="cursor-pointer"
-                    onClick={() => setIsCardMenuOpen(!isCardMenuOpen)}
-                    ref={refs.setReference}
-                    {...getReferenceProps()}
-                >
-                <BsThreeDots
-                    color="white"
-                />
-                </div>
+                <SimpleMenu>
+                    <li onClick={handleEditCard} className="text-[#5F6C72] py-2 cursor-pointer">Edit Card</li>
+                    <li className="text-[#5F6C72] py-2 cursor-pointer">Delete Card</li>
+                </SimpleMenu>
             </div>
             <div className="mt-3">
                 <p className="text-[11px] text-gray-200">CARD NUMBER</p>
@@ -55,20 +35,54 @@ function PaymentCard({ amount, cardNumber, cardHolder, cardType,  backgroundColo
                 }
                 <p className="text-white text-sm">{cardHolder}</p>
             </div>
-            {isCardMenuOpen &&
-                <div
-            ref={refs.setFloating}
-            style={floatingStyles}
-            {...getFloatingProps()}>
-                    <div className="bg-white">
-                        <ul className="flex flex-col gap-2 p-2">
-                            <li>Edit Card</li>
-                            <li>Delete Card</li>
-                        </ul>
-                    </div>
-            </div>}
         </div>
     );
+}
+
+
+
+// eslint-disable-next-line react/prop-types
+export const SimpleMenu = ({children}) => {
+    const [isCardMenuOpen, setIsCardMenuOpen] = useState(false);
+
+    const { refs, floatingStyles, context } = useFloating({
+        open: isCardMenuOpen,
+        onOpenChange: setIsCardMenuOpen,
+        placement: "bottom-start",
+    })
+
+    const click = useClick(context);
+    const dismiss = useDismiss(context);
+
+    const {getReferenceProps, getFloatingProps} = useInteractions([
+        click,
+        dismiss
+    ])
+
+    return (
+        <>
+            <div
+                className="cursor-pointer"
+                ref={refs.setReference}
+                {...getReferenceProps()}
+            >
+                <BsThreeDots
+                    color="white"
+                />
+            </div>
+            {isCardMenuOpen &&
+                <div
+                    ref={refs.setFloating}
+                    style={floatingStyles}
+                    {...getFloatingProps()}>
+                    <div className="bg-white rounded-sm shadow">
+                        <ul className="flex flex-col gap-2 p-3 items-start">
+                            {children}
+                        </ul>
+                    </div>
+                </div>}
+        </>
+    )
 }
 
 export default PaymentCard;
