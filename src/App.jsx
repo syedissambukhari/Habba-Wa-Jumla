8dbaa36bd30afadbd0e16bfb0ed5d9ce996c6d8e
SaadMustafa <saadtanoli445@gmail.com>
2024-10-10 06:02:12 -0700

    resolve errors

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import Breadcrumb from "./homecomponents/BreadCrumbs";
 import Header from "./homecomponents/Header";
 import ShopPage from "./pages/ShopPage";
-
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Shoping from "./pages/Shoping";
@@ -15,52 +14,59 @@ import CompareProduct from "./pages/CompareProduct";
 import FAQPage from "./pages/FAQPage.jsx";
 import DashboardBrowsingHistory from "./pages/DashboardBrowsingHistory.jsx";
 import { UserDashboard } from "./pages/Dashboard/UserDashboard.jsx";
-<<<<<<< HEAD
-// import { UserDashboard } from "./pages/Dashboard/UserDashboard.jsx";
-=======
 import TrackOrderDetails from "./homecomponents/TrackOrderDetails.jsx";
 import CardsAndAddressesPage from "./pages/Dashboard/CardsAndAddressesPage.jsx";
 import NotFoundPage from "./pages/NotFoundPage.jsx";
->>>>>>> 79bb7d5d2edd959144d1ab6507ba4776cfb5cbda
+
 function App() {
   return (
     <>
       <div>
         <Header />
-        {/* <Breadcrumb/> */}
-        {/* <HeroSection /> */}
-        {/* <ShopPage/> */}
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/Wishlist" element={<Wishlist/>} />
-          <Route path="/TrackOrder" element={<TrackOrder/>} />
-          <Route path="/CustomerSupport" element={<CustomerSupport />} /> {/* Fixed typo here */}
-          <Route path="/CompareProduct" element={<CompareProduct/>} />
-          <Route path="/browsing-history" element={<><Breadcrumb/><DashboardBrowsingHistory/></>} />
-<<<<<<< HEAD
-
-       
-=======
-          <Route path="/TrackOrderDetails" element={<TrackOrderDetails/>} />
-
->>>>>>> 79bb7d5d2edd959144d1ab6507ba4776cfb5cbda
-        </Routes>
-
-        <Routes>
-        <Route path="/UserDashboard" element={<UserDashboard/>} />
-          <Route path="/dashboard/billing" element={<CardsAndAddressesPage /> } />
-        </Routes>
-        <Routes>
-          
-          <Route path="/shop" element={<><Breadcrumb/><ShopPage/></>} />
+          <Route path="/Wishlist" element={<Wishlist />} />
+          <Route path="/TrackOrder" element={<TrackOrder />} />
+          <Route path="/CustomerSupport" element={<CustomerSupport />} />
+          <Route path="/CompareProduct" element={<CompareProduct />} />
+          <Route
+            path="/browsing-history"
+            element={
+              <>
+                <Breadcrumb />
+                <DashboardBrowsingHistory />
+              </>
+            }
+          />
+          <Route path="/TrackOrderDetails" element={<TrackOrderDetails />} />
+          <Route path="/UserDashboard" element={<UserDashboard />} />
+          <Route
+            path="/dashboard/billing"
+            element={<CardsAndAddressesPage />}
+          />
+          <Route
+            path="/shop"
+            element={
+              <>
+                <Breadcrumb />
+                <ShopPage />
+              </>
+            }
+          />
           <Route path="/faq" element={<FAQPage />} />
           <Route path="/not-found" element={<NotFoundPage />} />
+          <Route
+            path="/shopping-card"
+            element={
+              <>
+                <Breadcrumb />
+                <ShoppingCard />
+              </>
+            }
+          />
         </Routes>
-        <Routes>
-          <Route path="/shopping-card" element={<><Breadcrumb/><ShoppingCard/></>} />
-        </Routes>
-        <Footer/>
-        </div>
+        <Footer />
+      </div>
     </>
   );
 }
