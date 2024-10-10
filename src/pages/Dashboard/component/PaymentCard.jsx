bb0ac4c0eb4d788ca85a85f2191650f33e90d14c
Nabil <cleo.master0000@gmail.com>
2024-10-10 13:50:23 +0500

    Add Payment Card Menu

--- src/pages/Dashboard/component/PaymentCard.jsx
+++ src/pages/Dashboard/component/PaymentCard.jsx
@@ -2,14 +2,40 @@ import {BsThreeDots} from "react-icons/bs";
 import {PiCopyDuotone} from "react-icons/pi";
 import {RiVisaLine} from "react-icons/ri";
 import { FaCcMastercard } from "react-icons/fa6";
+import { useFloating, useDismiss, useInteractions } from "@floating-ui/react";
+import {useState} from "react";
 
 // eslint-disable-next-line react/prop-types
 function PaymentCard({ amount, cardNumber, cardHolder, cardType,  backgroundColorClassName }) {
+    const [isCardMenuOpen, setIsCardMenuOpen] = useState(false);
+
+    const { refs, floatingStyles, context } = useFloating({
+        open: isCardMenuOpen,
+        onOpenChange: setIsCardMenuOpen,
+        placement: "bottom-start",
+    })
+
+    const dismiss = useDismiss(context);
+
+    const {getReferenceProps, getFloatingProps} = useInteractions([
+        dismiss
+    ])
+
+
     return (
         <div className={`p-6 border rounded ${backgroundColorClassName}`}>
             <div className="flex items-center justify-between">
                 <p className="text-white text-[16px] leading-6">{amount}</p>
-                <BsThreeDots color="white"/>
+                <div
+                    className="cursor-pointer"
+                    onClick={() => setIsCardMenuOpen(!isCardMenuOpen)}
+                    ref={refs.setReference}
+                    {...getReferenceProps()}
+                >
+                <BsThreeDots
+                    color="white"
+                />
+                </div>
             </div>
             <div className="mt-3">
                 <p className="text-[11px] text-gray-200">CARD NUMBER</p>
@@ -29,6 +55,18 @@ function PaymentCard({ amount, cardNumber, cardHolder, cardType,  backgroundColo
                 }
                 <p className="text-white text-sm">{cardHolder}</p>
             </div>
+            {isCardMenuOpen &&
+                <div
+            ref={refs.setFloating}
+            style={floatingStyles}
+            {...getFloatingProps()}>
+                    <div className="bg-white">
+                        <ul className="flex flex-col gap-2 p-2">
+                            <li>Edit Card</li>
+                            <li>Delete Card</li>
+                        </ul>
+                    </div>
+            </div>}
         </div>
     );
 }
