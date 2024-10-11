089e525cf2da3cf63821d078fd6d48f61537af88
Huzaifa Sajjad <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-10-11 09:28:08 -0700

    add dashboard

--- src/pages/Dashboard/UserDashboard.jsx
+++ src/pages/Dashboard/UserDashboard.jsx
@@ -1,19 +1,12 @@
-<<<<<<< Updated upstream
 import React from "react";
 import Dashboard from "./Dashboard";
 import DashboardSider from "../../homecomponents/DashboardSider";
 import { Outlet } from "react-router-dom";
-=======
-import React from 'react'
-import Dashboard from './Dashboard'
-import DashboardSider from '../../homecomponents/DashboardSider'
->>>>>>> Stashed changes
 
 export const UserDashboard = () => {
   return (
     <div className="flex gap-5">
-<<<<<<< Updated upstream
-      <div className="max-w-[1320px] gap-2 h-[2320px] m-auto py-10 ">
+      <div className="max-w-[1320px] gap-2 h-full m-auto py-10 ">
         <div className="w-full h-full  flex gap-5 ">
           <div className="h-full basis-1/4 ">
             <DashboardSider active={"Dashboard"} />
@@ -21,15 +14,6 @@ export const UserDashboard = () => {
           <div className="h-full basis-3/4 ">
             <Dashboard />
           </div>
-=======
-<div className='max-w-[1320px] gap-2 h-full m-auto py-10 '>
-      <div className='w-full h-full  flex gap-5 '>
-        <div className='h-full basis-1/4 '>
-          <DashboardSider/>
-        </div>
-        <div className='h-full basis-3/4 '>
-        <Dashboard />
->>>>>>> Stashed changes
         </div>
       </div>
     </div>
