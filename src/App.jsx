3e150c4e2c1ce560e132e031a080a65422cc8ca8
Nabil <cleo.master0000@gmail.com>
2024-10-12 11:32:51 +0500

    Routing architecture changed from v5 to v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,15 @@
 import "./App.css";
-import Breadcrumb from "./homecomponents/BreadCrumbs";
 import Header from "./homecomponents/Header";
-import ShopPage from "./pages/ShopPage";
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+import {Outlet} from "react-router-dom";
 import Footer from "./homecomponents/Footer";
-import ShoppingCard from "./pages/ShoppingCard";
-import Wishlist from "./pages/Wishlist";
-import TrackOrder from "./homecomponents/TrackOrder";
-import CustomerSupport from "./homecomponents/CustomerSupport.jsx";
-import CompareProduct from "./pages/CompareProduct";
-import FAQPage from "./pages/FAQPage.jsx";
-import DashboardBrowsingHistory from "./pages/DashboardBrowsingHistory.jsx";
-import { UserDashboard } from "./pages/Dashboard/UserDashboard.jsx";
-import TrackOrderDetails from "./homecomponents/TrackOrderDetails.jsx";
-import CardsAndAddressesPage from "./pages/Dashboard/CardsAndAddressesPage.jsx";
-import NotFoundPage from "./pages/NotFoundPage.jsx";
-import OrderCheckout from "./pages/Checkoutpage.jsx";
-import OrderSuccess from "./pages/OrderSuccessful.jsx";
-import BlogList from "./homecomponents/BlogList.jsx";
-import BlogDetailsPage from "./pages/BlogDetailsPage.jsx";
+
 
 function App() {
   return (
     <>
       <div>
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Wishlist" element={<Wishlist />} />
-          <Route path="/TrackOrder" element={<TrackOrder />} />
-          <Route path="/CustomerSupport" element={<CustomerSupport />} />
-          <Route path="/CompareProduct" element={<CompareProduct />} />
-          <Route
-            path="/browsing-history"
-            element={
-              <>
-                <Breadcrumb />
-                <DashboardBrowsingHistory />
-              </>
-            }
-          />
-          <Route path="/TrackOrderDetails" element={<TrackOrderDetails />} />
-          <Route path="/UserDashboard" element={<UserDashboard />} />
-          <Route
-            path="/dashboard/billing"
-            element={<CardsAndAddressesPage />}
-          />
-          <Route
-            path="/shop"
-            element={
-              <>
-                <Breadcrumb />
-                <ShopPage />
-              </>
-            }
-          />
-          <Route path="/faq" element={<FAQPage />} />
-          <Route path="/not-found" element={<NotFoundPage />} />
-          <Route
-            path="/shopping-card"
-            element={
-              <>
-                <Breadcrumb />
-                <ShoppingCard />
-              </>
-            }
-          />
-          <Route path="/ordercheckout" element={<OrderCheckout />}></Route>
-          <Route path="/ordersuccess" element={<OrderSuccess />}></Route>
-          <Route path="/bloglist" element={<BlogList />}></Route>
-          <Route path="/blogdetails" element={<BlogDetailsPage />}></Route>
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+          <Outlet />
         <Footer />
       </div>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,122 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import OrderCheckout from "./homecomponents/OrderCheckout.jsx";
+import Home from "./pages/Home.jsx";
+import Wishlist from "./pages/Wishlist.jsx";
+import TrackOrder from "./homecomponents/TrackOrder.jsx";
+import CustomerSupport from "./homecomponents/CustomerSupport.jsx";
+import CompareProduct from "./pages/CompareProduct.jsx";
+import Breadcrumb from "./homecomponents/BreadCrumbs.jsx";
+import DashboardBrowsingHistory from "./pages/DashboardBrowsingHistory.jsx";
+import TrackOrderDetails from "./homecomponents/TrackOrderDetails.jsx";
+import {UserDashboard} from "./pages/Dashboard/UserDashboard.jsx";
+import CardsAndAddressesPage from "./pages/Dashboard/CardsAndAddressesPage.jsx";
+import ShopPage from "./pages/ShopPage.jsx";
+import FAQPage from "./pages/FAQPage.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
+import ShoppingCard from "./pages/ShoppingCard.jsx";
+import OrderSuccess from "./homecomponents/OrderSuccess.jsx";
+import BlogList from "./homecomponents/BlogList.jsx";
+import BlogDetailsPage from "./pages/BlogDetailsPage.jsx";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/Wishlist",
+                element: <Wishlist />,
+            },
+            {
+                path: "/TrackOrder",
+                element: <TrackOrder />,
+            },
+            {
+                path: "/CustomerSupport",
+                element: <CustomerSupport />,
+            },
+            {
+                path: "/CompareProduct",
+                element: <CompareProduct />,
+            },
+            {
+                path: "/browsing-history",
+                element: (
+                    <>
+                        <Breadcrumb />
+                        <DashboardBrowsingHistory />
+                    </>
+                ),
+            },
+            {
+                path: "/TrackOrderDetails",
+                element: <TrackOrderDetails />,
+            },
+            {
+                path: "/UserDashboard",
+                element: <UserDashboard />,
+            },
+            {
+                path: "/dashboard/billing",
+                element: <CardsAndAddressesPage />,
+            },
+            {
+                path: "/shop",
+                element: (
+                    <>
+                        <Breadcrumb />
+                        <ShopPage />
+                    </>
+                ),
+            },
+            {
+                path: "/faq",
+                element: <FAQPage />,
+            },
+            {
+                path: "/not-found",
+                element: <NotFoundPage />,
+            },
+            {
+                path: "/shopping-card",
+                element: (
+                    <>
+                        <Breadcrumb />
+                        <ShoppingCard />
+                    </>
+                ),
+            },
+            {
+                path: "/ordercheckout",
+                element: <OrderCheckout />,
+            },
+            {
+                path: "/ordersuccess",
+                element: <OrderSuccess />,
+            },
+            {
+                path: "/bloglist",
+                element: <BlogList />,
+            },
+            {
+                path: "/blogdetails",
+                element: <BlogDetailsPage />,
+            }
+        ],
+    },
+]);
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
