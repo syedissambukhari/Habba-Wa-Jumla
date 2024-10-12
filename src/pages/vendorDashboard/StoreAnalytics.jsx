1457e15ffbea5666bbd4a4328f443a78fb336616
ahtisham313 <130073639+ahtisham313@users.noreply.github.com>
2024-10-12 18:29:13 +0500

    add store analytics

--- src/pages/vendorDashboard/StoreAnalytics.jsx
+++ src/pages/vendorDashboard/StoreAnalytics.jsx
@@ -1,20 +1,32 @@
 import React from 'react'
 import { Stats } from './components/Stats'
+import SalesRevenue from './components/SalesRevenue'
+import TopSellingProducts from './components/TopSellingProducts'
+import SalesByCountry from './components/SalesByCountry'
+import CostBreakdown from './components/CostBreakdown'
 const StoreAnalytics = () => {
   return (
     <div>
          <div>
-        <div className="p-8 mt-16 w-full h-[72px]   justify-between ml-auto flex items-center space-x-4">
+        <div className="p-8 mt-16 w-full h-[72px]   justify-between ml-auto flex items-center space-x-4 ">
           <h1 className="text-[33px] font-bold font-plusjakartasans">Store Analytics</h1>
           {/* Other components can be added here */}
 
           <button className='w-[132px] h-[40px] rounded-[12px] bg-[#0B5D51] shadow-[0px_1px_5px_0px] shadow-[#00000014] flex items-center justify-center gap-2'>
-            <img src="reload.svg" alt="reload" />
+            <img src="/reload.svg" alt="reload" />
             <p className='font-inter font-semibold text-[14px] text-white leading-6'>Sync Data </p>
           </button>
         </div>
-        <div>
-        <Stats/>
+        <div className='flex  justify-center '>
+           <div>
+            <Stats/>
+            <SalesRevenue/>
+            <SalesByCountry/>
+           </div>
+          <div className=''>
+            <TopSellingProducts/>
+            <CostBreakdown/>
+          </div>
         </div>
     </div>
     </div>
--- src/pages/vendorDashboard/components/VendorHeader.jsx
+++ src/pages/vendorDashboard/components/VendorHeader.jsx
@@ -12,22 +12,22 @@ const VendorHeader = () => {
      <div className='ml-auto flex items-center space-x-4 '>
         {/* Placeholder for additional icons */}
         <div className='h-[49.6px] w-[212px] flex  justify-between pt-3'>
-          <img src="flag-uk.svg.svg" alt="flag"  className='w-7 h-7'/>
+          <img src="/flag-uk.svg.svg" alt="flag"  className='w-7 h-7'/>
           {/* <img src="notifi.svg" alt="notification"  />
           <img src="badge.svg" alt="badge" /> */}
           <div className="relative inline-block ">
   {/* Notification Icon */}
-  <img src="notifi.svg" alt="notification" className="w-6 h-6" />
+  <img src="/notifi.svg" alt="notification" className="w-6 h-6" />
 
   {/* Badge Icon */}
   <img 
-    src="badge.svg" 
+    src="/badge.svg" 
     alt="badge" 
     className="absolute top-[-6px] right-[-6px] w-4 h-4" 
   />
 </div>
-<img src="contac.svg" alt="contacts" className='w-6 h-6' />
-<img src="Avatar.svg" alt="avatar" className='w-8 h-8' />
+<img src="/contac.svg" alt="contacts" className='w-6 h-6' />
+<img src="/Avatar.svg" alt="avatar" className='w-8 h-8' />
 
         </div>
         {/* Add more icons here */}
