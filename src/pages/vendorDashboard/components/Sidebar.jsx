2b3f22643696a387fbc38da307ba8f04f1ede36d
SaadMustafa <saadtanoli445@gmail.com>
2024-10-14 02:28:26 -0700

    add a Side bar Product Items

--- src/pages/vendorDashboard/components/Sidebar.jsx
+++ src/pages/vendorDashboard/components/Sidebar.jsx
@@ -1,17 +1,18 @@
-import React, { useState } from 'react';
-import { FaChartBar, FaUser, FaBoxOpen, FaShoppingCart, FaFileInvoice, FaTruck, FaCalendarAlt, FaFileAlt, FaLock, FaSignOutAlt } from 'react-icons/fa';
-import { MdDashboard } from 'react-icons/md';
-import { IoAnalyticsSharp } from 'react-icons/io5';
-import { FiFileText } from 'react-icons/fi';
-import { HiOutlineChartSquareBar } from "react-icons/hi";
-import { PiShoppingCartDuotone } from "react-icons/pi";
-import { IoMdArrowDropright } from "react-icons/io";
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import vendorLogo from "../../../../public/logoo.svg";
-import { Link } from 'react-router-dom';
+// Import necessary icons
+import { PiShoppingCartDuotone } from "react-icons/pi";
 
-// import Logo from './assets/logo-2.png';
+// Sidebar component
 const Sidebar = () => {
+  const location = useLocation(); // Get current location
   const [isOrdersDropdownOpen, setIsOrdersDropdownOpen] = useState(false);
+  const [isProductOpen, setProductOpen] = useState(false);
+
+  const toggleProductOpen = () => {
+    setProductOpen(!isProductOpen);
+  };
 
   const toggleOrdersDropdown = () => {
     setIsOrdersDropdownOpen(!isOrdersDropdownOpen);
@@ -21,38 +22,74 @@ const Sidebar = () => {
     <div className="h-[1610px] w-[280px] bg-[#0B5D51] text-white flex flex-col items-start p-4">
       {/* Logo Section */}
       <div className="mt-[9px] ml-[35px] w-[95px] h-[86px]">
-        <img src={vendorLogo} alt="Logo" className='' />
+        <img src={vendorLogo} alt="Logo" />
       </div>
 
       {/* Store Analytics */}
       <div className="flex items-center bg-[#073741] w-[247.2px] h-[36px] mt-6 left-[16px] p-[6px_16px] gap-0 rounded-lg ">
-        <img src='store.svg' className="w-5 h-5 top-2 mr-4" />
-        <span className='font-inter font-semibold text-[14px] leading-6'>Store Analytics</span>
+        <img src="store.svg" className="w-5 h-5 top-2 mr-4" />
+        <span className="font-inter font-semibold text-[14px] leading-6">
+          Store Analytics
+        </span>
       </div>
 
       {/* Menu Items */}
       <nav className="flex-grow space-y-4 ml-2 mt-10">
-        <SidebarItem imgSrc='/accountSVG.svg' text="Account" />
-        <SidebarItem imgSrc='/customer.svg' text="Customers" badge="New" />
-        <SidebarItem imgSrc='/product.svg' text="Products" />
+        <SidebarItem imgSrc="/accountSVG.svg" text="Account" />
+        <SidebarItem imgSrc="/customer.svg" text="Customers" badge="New" />
+        {/* Products dropdown */}
+        <SidebarItem
+          imgSrc="/product.svg"
+          text="Products"
+          onClick={toggleProductOpen}
+        />
+        {isProductOpen && (
+          <div className="ml-8 mt-2 space-y-3">
+            {" "}
+            <Link to="/vendor/productlists">
+              <DropdownItem
+                text="Product List"
+                isActive={location.pathname === "/vendor/productlists"}
+              />
+            </Link>
+            <Link to="/vendor/productcreates">
+              <DropdownItem
+                text="Product Create"
+                isActive={location.pathname === "/vendor/productcreates"}
+              />
+            </Link>
+          </div>
+        )}
+
         {/* Orders Item with Dropdown */}
-        <SidebarItem imgSrc='/orders.svg' text="Orders" onClick={toggleOrdersDropdown}>
+        <SidebarItem
+          imgSrc="/orders.svg"
+          text="Orders"
+          onClick={toggleOrdersDropdown}
+        >
           {isOrdersDropdownOpen && (
             <div className="ml-8 mt-2 space-y-2">
-             <Link to="/vendor/OrderList"> <DropdownItem text="List" /></Link>
+              <Link to="/vendor/OrderList">
+                <DropdownItem
+                  text="List"
+                  isActive={location.pathname === "/vendor/OrderList"}
+                />
+              </Link>
               <DropdownItem text="Details" />
               <DropdownItem text="Order status" />
-        
             </div>
           )}
         </SidebarItem>
-        <SidebarItem imgSrc='/invoices.svg' text="Invoices" />
-        <SidebarItem imgSrc='/logistics.svg' text="Logistics" />
-        <SidebarItem imgSrc='/blog.svg' text="Blog" />
-        <SidebarItem imgSrc='/calender.svg' text="Calendar" />
-        <SidebarItem imgSrc='/fileManager.svg' text="File Manager" />
-        <SidebarItem imgSrc='/Auth.svg' text="Auth" />
-        <SidebarItem icon={<PiShoppingCartDuotone className='w-5 h-[18px]' />} text="Checkout" />
+        <SidebarItem imgSrc="/invoices.svg" text="Invoices" />
+        <SidebarItem imgSrc="/logistics.svg" text="Logistics" />
+        <SidebarItem imgSrc="/blog.svg" text="Blog" />
+        <SidebarItem imgSrc="/calender.svg" text="Calendar" />
+        <SidebarItem imgSrc="/fileManager.svg" text="File Manager" />
+        <SidebarItem imgSrc="/Auth.svg" text="Auth" />
+        <SidebarItem
+          icon={<PiShoppingCartDuotone className="w-5 h-[18px]" />}
+          text="Checkout"
+        />
       </nav>
     </div>
   );
@@ -70,8 +107,6 @@ const SidebarItem = ({ imgSrc, icon, text, badge, children, onClick }) => (
       {/* Render icon if provided */}
       {icon && <div className="w-5 h-[18px]">{icon}</div>}
       <span className="flex-grow">{text}</span>
-      {/* Render badge if provided */}
-      {badge && <span className="bg-[#6366F1] text-[13px] w-[44px] h-[22px] text-white px-2 flex items-center rounded-[16px]">{badge}</span>}
     </div>
     {/* Render dropdown content if there are children */}
     {children}
@@ -79,8 +114,12 @@ const SidebarItem = ({ imgSrc, icon, text, badge, children, onClick }) => (
 );
 
 // Dropdown Item Component
-const DropdownItem = ({ text }) => (
-  <div className="text-sm text-white hover:text-gray-300 cursor-pointer">{text}</div>
+const DropdownItem = ({ text, isActive }) => (
+  <div className="flex items-center text-sm text-white hover:text-gray-300 cursor-pointer">
+    {isActive && <div className="w-2 h-2 bg-blue-500 rounded-full mr-2" />}{" "}
+    {/* Blue dot for active item */}
+    {text}
+  </div>
 );
 
 export default Sidebar;
